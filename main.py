9557b816e241996a84b47406434a714eb68da695
desvasicek <desmond.vasicek@protonmail.com>
2022-02-21 11:56:51 -0800

    v1.1 now available on all platforms

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ recentpaths = []
 class File:
     def __init__(self, row, column, filepath, filename):
         if os.path.isfile(filepath):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/blank.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/blank.png")
         else:
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/Folder-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/Folder-0.png")
             if filename == "Downloads":
-                self.image = PhotoImage(file="/home/pi/File Manager/icons/Folder Downloads-0.png")
+                self.image = PhotoImage(file=os.getcwd() + "/icons/Folder Downloads-0.png")
         if filename.endswith(".zip"):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/Zip-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/Zip-0.png")
         elif filename.endswith(".exe"):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/exe-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/exe-0.png")
         elif filename.endswith(".mp3") | filename.endswith(".wav") | filename.endswith(".ogg"):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/Audio-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/Audio-0.png")
         elif filename.endswith(".mp4") | filename.endswith(".vid"):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/Video file-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/Video file-0.png")
         elif filename.endswith(".png") | filename.endswith(".jpg") | filename.endswith(".jpeg") | filename.endswith(".ico"):
-            self.image = PhotoImage(file="/home/pi/File Manager/icons/Photos file-0.png")
+            self.image = PhotoImage(file=os.getcwd() + "/icons/Photos file-0.png")
         self.btn = Button(app, text=filename, command=lambda:self.openfile(), image=self.image, compound="top")
         self.btn.grid(row=row, column=column)
         self.filename = filename
@@ -61,15 +61,15 @@ app = Tk()
 app.title("File Manager")
 app.geometry("1050x1050")
 app.config(bg="white")
-app.call("source", "/home/pi/File Manager/arc/arc.tcl")
-app.iconphoto(False, PhotoImage(file="/home/pi/File Manager/icons/Folder-0.png"))
+app.call("source", os.getcwd() + "/arc/arc.tcl")
+app.iconphoto(False, PhotoImage(file=os.getcwd() + "/icons/Folder-0.png"))
 Style(app).theme_use("arc")
 Button(app, text="<-- Back <--", command=goback).grid(row=0, column=0,ipadx=5, ipady=15)
 pathvar = StringVar()
 pathentry = Entry(app, textvariable=pathvar)
 pathentry.bind('<KeyRelease-Return>', goto)
 pathentry.grid(row=0, column=1, ipadx=5, ipady=15)
-path = "/home/pi/"
+path = os.getcwd()
 def opendir():
     global files, pathvar
     pathvar.set(path)
